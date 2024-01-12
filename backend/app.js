2f60460522b526f3083cc90f4807be2d50df36c2
Inferno-coder <pradeepceo18@gmail.com>
2024-01-12 21:44:24 +0530

    a

--- backend/app.js
+++ backend/app.js
@@ -22,9 +22,9 @@ app.use('/api/v1/',order);
 app.use('/api/v1/',payment);
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.use(express.static(path.join('https://project-nfu1.onrender.com', '../frontend/build')));
     app.get('*', (req, res) =>{
-        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+        res.sendFile(path.resolve('https://project-nfu1.onrender.com', '../frontend/build/index.html'))
     })
 }
 
